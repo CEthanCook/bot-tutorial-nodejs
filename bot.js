4cf941d0fc95be5b3f771d7781028a5d9ce6d6ec
Creighton Ethan Cook <creigh3@clemson.edu>
2018-01-23 13:26:37 -0500

    Still deciphering

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^.*\@[Qq]uote\s*[Bb]ot.*/;
 
-  console.log(request);
+  console.log(request.attachments);
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
